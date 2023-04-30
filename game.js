03924a9f097afdf0c646d33f9f9f8b7954fd8481
coreglitch <mail@s3f.ru>
2023-04-30 13:28:36 +0400

    pass texture to shader

--- game.js
+++ game.js
@@ -3,31 +3,43 @@ import { SpatialControls } from "spatial-controls";
 import { Player } from "./player";
 import { ClickMove } from './click_move';
 
+const vertexShader = `
+varying vec2 vuv;
+
+void main() {
+    vuv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
+
+const fragmentShader = `
+varying vec2 vuv;
+uniform sampler2D in_texture;
+uniform float time;
+
+void main() {
+    vec4 color = texture(in_texture, vuv + vec2(0., sin(time + vuv.y) * vuv.y));
+    gl_FragColor = vec4(color.r, 0.0, sin(time + vuv.x * 100.), 1.0);
+}
+`;
+
 function game(app) {
-    /*(new THREE.TextureLoader()).load("assets/sky.jpg", (texture) => {
-        app.getSceneObject("sky").material = new THREE.MeshBasicMaterial({map: texture});
-    });*/
+    (new THREE.TextureLoader()).load("assets/sky.jpg", (texture) => {
+        // app.getSceneObject("sky").material = new THREE.MeshBasicMaterial({map: texture});
+        let material = new THREE.ShaderMaterial({
+            uniforms: {
+                in_texture: {value: texture},
+                time: {value: 0},
+            },
+            fragmentShader: fragmentShader,
+            vertexShader: vertexShader
+        });
 
-    const vertexShader = `
-    varying vec2 vuv;
+        app.setEventHandler("update", ({time, _delta}) => {
+            material.uniforms.time.value = time / 1000;
+        });
 
-    void main() {
-        vuv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-    `;
-
-    const fragmentShader = `
-    varying vec2 vuv;
-
-    void main() {
-        gl_FragColor = vec4(0.0, 0.0, sin(vuv.x * 100.), 1.0);
-    }
-    `;
-
-    app.scene.getObjectByName("sky").material = new THREE.ShaderMaterial({
-        fragmentShader: fragmentShader,
-        vertexShader: vertexShader
+        app.scene.getObjectByName("sky").material = material;
     });
 
     const { position, quaternion } = app.camera;
